0c6dc920bc0ae3ddf91dc3abfe9ff4ae74212601
Nicholasn <nicholasn@Huxley-007.local>
2019-09-27 08:08:17 -0700

    adding parts of the card class

--- resources/card.py
+++ resources/card.py
@@ -1,7 +1,6 @@
 from enum import Enum, auto
 
 class Card:
-    
 
     # class ("static") members and indented constants
     class CardValue(Enum):
@@ -35,23 +34,42 @@ class Card:
             ret_srt = self.name[0].upper() + self.name[1:].lower()
             return ret_srt
 
+    def set_val(self, val):
+        if(val == 'TWO'):
+            return self.CardValue.TWO
+    
+    def get_val(self):
+        return self.__value
+
     DEFAULT_VAL = CardValue.ACE
     DEFAULT_SUIT = CardSuit.SPADES
+    __value = 0
+    __suit = 0
 
-    def __init__(self, the_value=DEFAULT_VAL, the_suit=DEFAULT_SUIT):
-        print(self.DEFAULT_SUIT)
+    # print(__value)
+
+    def __init__(self, val=DEFAULT_VAL, suit=DEFAULT_SUIT):
+        # print(val)
+        self.__value = self.set_val(val)
+        # print(self.__value)
         print("card made")
+        # print(self.DEFAULT_SUIT.name)
 
-    def __str__(self):
-        # ret_str = self.name[0].upper() + self.name[1:].lower()
-        ret_str = "place holder stringizer"
 
-        return ret_str
+    # def __str__(self):
+    #     # ret_str = self.name[0].upper() + self.name[1:].lower()
+    #     ret_str = "place holder stringizer"
+
+    #     return ret_str
 
 
 def main():
     test1 = Card()
-    print(test1.CardSuit.value)
+    test2 = Card("TWO", Card.CardSuit.CLUBS)
+    # print(test1.__value)
+    # print(test1.suit)
+    print(test2.get_val())
+
 
 
 if __name__ == "__main__":
