2cd80021a9df7cafeb3a492b0a21b071d456e943
Nicholas <nnlaor@gmail.com>
2022-01-23 19:34:24 -0800

    starting up the deck class

--- resources/deck.py
+++ resources/deck.py
@@ -4,10 +4,28 @@ This class will represent a deck of cards
 from card import Card
 
 
+
+# maybe implement a queue with a linked list
+class LinkedList:
+    def __init__(self):
+        self.head = Card()
+
 class Deck:
     def __init__():
         # instantiage whole deck of cards
+        # 52 cards in a deck
+        # 13 cards in a suit
         pass
+
+    def gen_hearts():
+        pass
+    def gen_diamonds():
+        pass
+    def gen_spades():
+        pass
+    def gen_clubs():
+        pass
+
 
 
 def main():
