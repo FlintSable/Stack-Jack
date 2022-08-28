ca598f443d6684221697b393767676ddb86c0659
Nicholas <nnlaor@gmail.com>
2022-08-27 23:50:08 -0400

    building out the stack hack class and stack_jack_game function

--- resources/stackjack.py
+++ resources/stackjack.py
@@ -1,15 +1,18 @@
 # this is the game class - game needs:
     # deck
 from deck import *
+from hand import Hand
 import random
 # from abc import ABC, abstractmethod
 # this file should probably have the StackJack game class
 
-class Player(Stack):
+class Player(Hand):
     def __init__(self, name):
+        Hand.__init__(self)
         self._name = name
-        Stack.__init__(self)
-        # maybe put the hand up here
+        
+        # Stack.__init__(self)
+        # maybe put the hand up here, does not need to be a stack
     
     @property
     def name(self):
@@ -24,10 +27,25 @@ class PlayerStandard(Player): # player - standard
 
 
 class PlayerDealer(Player): # player - dealer
-    def __init__(self, name="Dealer", table_players=[]):
+    def __init__(self,playing_deck, table_players=[], name="Dealer"):
+        if name != "Dealer":
+            name += " Dealer"
+            print("Custom Dealer Name: " + name)
         Player.__init__(self, name)
-        # print(name)
-        # new_deck = Deck()
+        self._activeDeck = playing_deck
+
+    @property
+    def activeDeck(self):
+        return self._activeDeck
+
+    @activeDeck.setter
+    def activeDeck(self, newdeck):
+        self._activeDeck = newdeck
+
+    @classmethod
+    def deal(cls):
+        print("cls: " + cls)
+        return 0
     
         # this role is almost like a game class
         # controls house hand
@@ -38,21 +56,45 @@ class PlayerDealer(Player): # player - dealer
         # show cards
 
 class StackJack:
+
     def __init__(self, tablePlayers):
+        # print(tablePlayers)
         self._deck = Deck()
-        self._dealer = PlayerDealer()
-        self._tablePlayers = tablePlayers
+        self._table_players = tablePlayers
+        self._dealer = PlayerDealer(self.deck, self._table_players, name="Delone")
+
+    @property
+    def deck(self):
+        return self._deck
+
+    @deck.setter
+    def deck(self, newdeck):
+        self._deck = newdeck
+
+    @property
+    def table_players(self):
+        return self._table_players
+    
+    @table_players.setter
+    def table_players(self,tablePlayers):
+        self._table_players = tablePlayers
+
+    def dealer_deal(self):
+        self._dealer.deal
+    
 
 def stack_jack_game(playerList):
-    current_player = playerList[0]
+    # current_player = playerList[0]
     start_game = StackJack(playerList)
-    # init_dealer = PlayerDealer()
-    print("stack jack game starting")
+    print(start_game.deck.pop().data.card)
+
+    
 
 
     while True:
         try:
-            print("try some stuff")
+            print("stack jack game starting")
+            break
         except(ValueError, IndexError) as e:
             print(e)
             continue
@@ -68,8 +110,8 @@ def stack_jack_game(playerList):
 
 def main():
     
-    p1 = PlayerStandard()
-    p2 = PlayerStandard()
+    p1 = PlayerStandard("Jeff-p1")
+    p2 = PlayerStandard("Sara-p2")
     current_players = [p1, p2]
     stack_jack_game(current_players)
 
